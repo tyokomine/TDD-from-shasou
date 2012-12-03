d7533fb8470287e8d9602f57c9e120b9f7314067
tatsuki yokomine <z.ibra.veste@gmail.com>
2012-12-03 21:53:13 +0900

    add empty error

--- tdd-shashou/train.rb
+++ tdd-shashou/train.rb
@@ -1,6 +1,38 @@
 class Stack
+  @value
+  def initialize
+    @value = []
+  end
+
   def empty?
     return true
   end
+  
+  def push(i)
+    @value << i
+  end
+
+  def top
+    empty_error
+    return @value[0]
+  end
+
+  def size
+    return @value.size
+  end
+
+  def pop
+    empty_error
+    return @value.pop
+  end
+
+  def empty_error
+    if @value.empty?
+      raise 'empty error'
+    end
+  end
+  private :empty_error
+
+
 end
 
--- tdd-shashou/train_spec.rb
+++ tdd-shashou/train_spec.rb
@@ -2,8 +2,53 @@ require './train'
 
 describe "train" do
   let(:stack){Stack.new}
-  describe "first test" do
+  describe "create test" do
     it {stack.empty?.should == true}
+  end
+  describe "push and top test" do
+    it "push 1 then top eq 1" do
+      stack.push(1)
+      stack.top.should == 1
+    end
+  end
+  describe "push1 and push2 then top eq 2" do
+    it "push 1 then top eq 1" do
+      stack.push(1)
+      stack.push(2)
+      stack.top.should == 1
+    end
+  end
+  describe "push and size test" do
+    it "push 1 then size eq 1" do
+      stack.push(1)
+      stack.size.should == 1
+    end
+  end
+  describe "push and size test2" do
+    it "push 1 and push 1 then size eq 2" do
+      stack.push(1)
+      stack.push(1)
+      stack.size.should == 2
+    end
+  end
+  describe "pop empty test" do
+    it "pop eq exception when no push" do
+      proc{stack.pop}.should raise_error RuntimeError,'empty error'
+    end
+  end
+  describe "top empty test" do
+    it "top eq exception when no push" do
+      proc{stack.top}.should raise_error RuntimeError,'empty error'
+    end
+  end
+
+  describe "pop test" do
+    it "push and pop test" do
+      stack.push(1)
+      stack.push(2)
+      stack.pop.should == 2
+      stack.pop.should == 1
+    end
   end
 end
 
